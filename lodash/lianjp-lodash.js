25d35edc70bb5e199580e5041529953c1fddc7ca
lianjp <lianjp23@gmail.com>
2018-07-17 17:32:04 +0800

    lodash

--- lodash/lianjp-lodash.js
+++ lodash/lianjp-lodash.js
@@ -3,11 +3,13 @@ var lianjp = {
 
   },
 
-  compact: function(array) {
+  compact: function (array) {
     var l=array.length
     for(i=0;i<l;i++) {
-      if(array[i]===false||array[i]===null||array[i]===0||array[i]===""||array[i]===''||array[i]===null||!isNaN(array[i])) {
+      if(array[i]===false||array[i]===null||array[i]===0||array[i]===""||array[i]===''||array[i]===null||isNaN(array[i])) {
         array.splice(i,1)
+        l=l-1
+        i=i-1
       }
     }
     return array
