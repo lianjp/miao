ed52548a083ed1d8c0fc0aea2662eeee01374787
lianjp <lianjp23@gmail.com>
2018-07-17 17:25:33 +0800

    lodash

--- lodash/lianjp-lodash.js
+++ lodash/lianjp-lodash.js
@@ -1,5 +1,15 @@
 var lianjp = {
   chunk: function(array, size) {
 
-  }
+  },
+
+  compact: function(array) {
+    var l=array.length
+    for(i=0;i<l;i++) {
+      if(array[i]===false||array[i]===null||array[i]===0||array[i]===""||array[i]===''||array[i]===null||!isNaN(array[i])) {
+        array.splice(i,1)
+      }
+    }
+    return array
+  },
 }
