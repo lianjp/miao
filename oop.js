3c07061fe1452a276e5fd74f5401e0a0668996bf
lianjp <lianjp23@gmail.com>
2018-07-31 23:00:42 +0800

    opp

--- oop.js
+++ oop.js
@@ -1,29 +1,37 @@
-function MyArray(ary) {
-  this.ary=ary  
+function MyArray(...value) {
+  var l=arguments.length
+  for(i=0;i<l;i++) {
+    this[i]=arguments[i]
+  }
+  this.length=l
 }
 
-Array.prototype = {
-  length: function(){
-    var a=this.ary
-    var count=0
-    for(var item in a) {
-      count++
+MyArray.prototype = {
+
+  push: function(...value) {
+    var l=this.length   
+    var l2=arguments.length
+    var l3=l+l2
+    for(i=l,j=0;i<l3;i++) {
+      this[i]=arguments[j]
+      this.length++
+      j++
     }
-    return count
+    return this.length
   },
 
   concat: function(...val) {    
     var l=arguments.length
     for(j=0;j<l;j++) {      
-      this.ary.push(arguments[j])
+      this.push(arguments[j])
     }
-    return this.ary
+    return this
   },
 
   every: function(f) {
-    var l=this.length()
+    var l=this.length
     for(i=0;i<l;i++) {
-      if(f(this.ary[i])){
+      if(f(this[i])){
         continue
       }else {
         return false
@@ -33,28 +41,28 @@ Array.prototype = {
   },
 
   filter: function(f) {
-    var var ary=[]
-    l=this.length()
+    var ary=[]
+    var l=this.length
     for(i=0;i<l;i++) {
-      if(f(this.ary[i])) {
-        ary.push(this.ary[i])
+      if(f(this[i])) {
+        ary.push(this[i])
       }
     }
     return ary
   },
 
   forEach: function(f) {
-    var l=this.length()
+    var l=this.length
     for(i=0;i<l;i++) {
-      this.ary[i]=f(this.ary[i])
+      f(this[i],i,this)
     }
-    return this.ary
+    return this
   },
 
   indexOf: function(value,start = 0) {
-    var l=this.length()
+    var l=this.length
     for(i=start;i<l;i++) {
-      if(this.ary[i]===value) {
+      if(this[i]===value) {
         return i
       }
     }
@@ -62,18 +70,18 @@ Array.prototype = {
   },
 
   join: function(separator = ',') {
-    var str=this.ary[0]
-    l=this.length()
+    var str=this[0]
+    l=this.length
     for(i=1;i<l;i++) {
-      str=str+separator+this.ary[i]
+      str=str+separator+this[i]
     }
     return str
   },
 
   lastIndexOf: function(value,start = 0) {
-    var l=this.length()
+    var l=this.length
     for(i=l-1;i>=0;i--) {
-      if(this.ary[i]===value) {
+      if(this[i]===value) {
         return i
       }
     }
@@ -82,35 +90,27 @@ Array.prototype = {
 
   map: function(f) {
     var ary=[]
-    l=this.length()
+    l=this.length
     for(i=0;i<l;i++) {
-      ary[i]=f(this.ary[i])
+      ary[i]=f(this[i])
     }
     return ary
   },
 
   pop: function() {
-    var l=this.length()    
-    var a=this.ary[l-1]
-    this.ary.length=l-1
+    var l=this.length    
+    var a=this[l-1]    
+    delete(this[l-1])
+    this.length=l-1
     return a
   },
 
-  push: function(...value) {
-    var l=this.length()    
-    var l2=arguments.length
-    var l3=l+l2
-    for(i=l,j=0;i<l3;i++) {
-      this.ary[i]=arguments[j]
-      j++
-    }
-    return this.length()
-  },
 
-  reduce: function(f) {
-    var l=this.length()
+
+  reduce: function(f,initialValue = this[0]) {
+    var l=this.length
     for(i=0;i<l;i++) {
-      
+      f(this[i])
     }
   },
 
@@ -119,28 +119,31 @@ Array.prototype = {
   },
 
   reverse: function() {
-    var ary=[]
-    var l=this.length()
+    var ary=new MyArray()
+    var l=this.length
     for(i=l-1,j=0;i>=0;i--) {
-      ary[j]=this.ary[i]
+      ary[j]=this[i]
       j++
     }
-    this.ary=ary
+    for(j=0;j<l;j++) {
+      this(j)=ary[j]
+    }
   },
 
   shift: function(value) {
-    var l=this.length()
-    var a=this.ary[0] 
+    var l=this.length
+    var a=this[0] 
     for(i=0;i<l-1;i++) {
-      this.ary[i]=this.ary[i+1]      
+      this[i]=this[i+1]      
     } 
-    this.ary.length=l-1
+    delete(this[l-1])
+    this.length=l-1
     return a  
   },
 
-  slice: function(start = 0, end = this.length()) {
+  slice: function(start = 0, end = this.length) {
     var ary=[]
-    var l=this.length()
+    var l=this.length
     if(start<0 && end>0){
       start=l+start      
     }else if(start>0 && end<0) {
@@ -150,7 +153,7 @@ Array.prototype = {
       end=l+end      
     }
     for(i=start,j=0;i<end;i++) {
-      ary[j]=this.ary[i]
+      ary[j]=this[i]
       j++
     }
     
@@ -158,9 +161,9 @@ Array.prototype = {
   },
 
   some: function(f) {
-    var l=this.length()
+    var l=this.length
     for(i=0;i<l;i++) {
-      if(f(this.ary[i])){
+      if(f(this[i])){
         return true
       }else {
         continue
@@ -201,25 +204,31 @@ Array.prototype = {
   },
 
   toString: function() {
-    var str=this.ary[0]
-    var l=this.length()
+    var str=this[0]
+    var l=this.length
     for(i=1;i<l;i++) {
-      str=str+','+this.ary[i]
+      str=str+','+this[i]
     }
     return str
   },
 
   unshift: function(...value) {
     var ary=[...value]
-    var l = this.length()
+    var l = this.length
     for(i=0;i<l;i++) {
-      ary.push(this.ary[i])
+      ary.push(this[i])
     }
-    this.ary=ary
-    return this.length()
+    var l2=ary.length
+    for(i=0;i<l2;i++) {
+      this[i]=ary[i]
+    }
+    this.length=l2
+    return this
   },
 
 
+}
 
-
+function MySet(ary) {
+  
 }
